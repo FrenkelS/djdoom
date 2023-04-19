89d4c855a2ac24f44d87327480dfc0973c691b23
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2023-04-19 18:42:46 +0200

    compile with LTO

--- compdj.bat
+++ compdj.bat
@@ -1,3 +1,8 @@
 mkdir DJDM19
-gcc -DAPPVER_EXEDEF=DM19 dmx.c i_main.c i_ibm.c i_sound.c tables.c f_finale.c d_main.c d_net.c g_game.c m_menu.c m_misc.c am_map.c p_ceilng.c p_doors.c p_enemy.c p_floor.c p_inter.c p_lights.c p_map.c p_maputl.c p_plats.c p_pspr.c p_setup.c p_sight.c p_spec.c p_switch.c p_mobj.c p_telept.c p_tick.c p_user.c r_bsp.c r_data.c r_draw.c r_main.c r_plane.c r_segs.c r_things.c w_wad.c wi_stuff.c v_video.c st_lib.c st_stuff.c hu_stuff.c hu_lib.c s_sound.c z_zone.c info.c sounds.c dutils.c -O3 -mtune=i386 -fomit-frame-pointer -funroll-loops -ffast-math -fstrength-reduce -o DJDM19/djdoom.exe
+
+set CFLAGS=-O3 -mtune=i386 -flto -fomit-frame-pointer -funroll-loops -ffast-math -fstrength-reduce
+rem set CFLAGS=%CFLAGS% -Wall
+
+set GLOBOBJS=dmx.c i_main.c i_ibm.c i_sound.c tables.c f_finale.c d_main.c d_net.c g_game.c m_menu.c m_misc.c am_map.c p_ceilng.c p_doors.c p_enemy.c p_floor.c p_inter.c p_lights.c p_map.c p_maputl.c p_plats.c p_pspr.c p_setup.c p_sight.c p_spec.c p_switch.c p_mobj.c p_telept.c p_tick.c p_user.c r_bsp.c r_data.c r_draw.c r_main.c r_plane.c r_segs.c r_things.c w_wad.c wi_stuff.c v_video.c st_lib.c st_stuff.c hu_stuff.c hu_lib.c s_sound.c z_zone.c info.c sounds.c dutils.c
+gcc -DAPPVER_EXEDEF=DM19 %GLOBOBJS% %CFLAGS% -o DJDM19/djdoom.exe
 strip -s DJDM19/djdoom.exe
