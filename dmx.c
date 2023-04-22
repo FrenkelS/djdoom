e35f1ae88a6ba7aa7f85f811e9bc5f681122e892
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2023-04-22 23:51:48 +0200

    revert DMX changes so it's easier to reinsert the real DMX

--- dmx.c
+++ dmx.c
@@ -19,21 +19,21 @@
 void _dpmi_lockregion (void * inmem, int length) {}
 void _dpmi_unlockregion (void * inmem, int length) {}
 
-void TSM_Install(int sndTicrate) {}
-int TSM_NewService(void (*timerISR)(void), int frequency) {return 0;}
+int TSM_Install(unsigned int sndTicrate) {return 0;}
+int TSM_NewService(int (*timerISR)(void), int frequency, int priority, int pause) {return 0;}
 void TSM_DelService(int tsm_ID) {}
 void TSM_Remove(void) {}
 
-void MUS_PauseSong(int handle) {}
-void MUS_ResumeSong(int handle) {}
+int MUS_PauseSong(int handle) {return 0;}
+int MUS_ResumeSong(int handle) {return 0;}
 void MUS_SetMasterVolume(int volume) {}
-int MUS_RegisterSong(void *data) {return 0;}
-void MUS_UnregisterSong(int handle) {}
-void MUS_StopSong(int handle) {}
-void MUS_ChainSong(int handle, int to) {}
-void MUS_PlaySong(int handle, int volume) {}
+int MUS_RegisterSong(unsigned char *data) {return 0;}
+int MUS_UnregisterSong(int handle) {return 0;}
+int MUS_StopSong(int handle) {return 0;}
+int MUS_ChainSong(int handle, int to) {return 0;}
+int MUS_PlaySong(int handle, int volume) {return 0;}
 
-int SFX_PlayPatch(void *data, int sep, int pitch, int volume) {return -1;}
+int SFX_PlayPatch(void *data, int pitch, int sep, int volume, int flags, int priority) {return -1;}
 void SFX_StopPatch(int handle) {}
 int SFX_Playing(int handle) {return 0;}
 void SFX_SetOrigin(int handle, int pitch, int sep, int volume) {}
@@ -42,14 +42,14 @@ int ENS_Detect(void) {return -1;}
 int CODEC_Detect(int *sbPort, int *sbDma) {return -1;}
 int GF1_Detect(void) {return -1;}
 void GF1_SetMap(char *dmxlump, int size) {}
-int SB_Detect(int *sbPort, int *sbIrq, int *sbDma) {return -1;}
+int SB_Detect(int *sbPort, int *sbIrq, int *sbDma, unsigned short *version) {return -1;}
 void SB_SetCard(int iBaseAddr, int iIrq, int iDma) {}
-int AL_Detect(int *wait) {return -1;}
+int AL_Detect(int *wait, int *type) {return -1;}
 void AL_SetCard(int wait, void *genmidi) {}
-int MPU_Detect(int *mPort) {return -1;}
+int MPU_Detect(int *mPort, int *type) {return -1;}
 void MPU_SetCard(int mPort) {}
 
-int DMX_Init(int ticrate, int maxsongs, int musicDevice, int sfxDevice) {return 0;}
+int DMX_Init(int ticrate, int maxsongs, unsigned int musicDevice, unsigned int sfxDevice) {return 0;}
 void DMX_DeInit(void) {}
 
-void WAV_PlayMode(int channels, int sampleRate) {}
+void WAV_PlayMode(int channels, unsigned short sampleRate) {}
