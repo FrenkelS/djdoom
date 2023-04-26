14504e7249b12869d351cecb9b042f76ea590b21
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2023-04-26 19:07:08 +0200

    reverse R_PointOnSide() change

--- r_main.c
+++ r_main.c
@@ -160,12 +160,16 @@ boolean	R_PointOnSegSide (fixed_t x, fixed_t y, seg_t *line)
 
 // try to quickly decide by looking at sign bits
 	if ( (ldy ^ ldx ^ dx ^ dy)&0x80000000 )
-		return (ldy ^ dx) & 0x80000000;
+	{
+		if ( (ldy ^ dx) & 0x80000000 )
+			return 1;	// (left is negative)
+		return 0;
+	}
 
 	left = FixedMul ( ldy>>FRACBITS , dx );
 	right = FixedMul ( dy , ldx>>FRACBITS );
 
-	return !(right < left);
+	return right >= left;
 }
 
 
