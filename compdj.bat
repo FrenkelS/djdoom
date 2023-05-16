4bcbafffa1b538dff44d46efd36716f3a2433e81
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2023-05-16 21:44:46 +0200

    DJGPP: better build flags

--- compdj.bat
+++ compdj.bat
@@ -4,7 +4,7 @@ mkdir DJDM19
 
 nasm planar.asm -f coff
 
-set CFLAGS=-O3 -march=i386 -flto -fwhole-program -fomit-frame-pointer -funroll-loops -Wno-attributes -Wpedantic
+set CFLAGS=-Ofast -march=i386 -flto -fwhole-program -fomit-frame-pointer -funroll-loops -fgcse-sm -fgcse-las -fipa-pta -mpreferred-stack-boundary=2 -Wno-attributes -Wpedantic
 @rem set CFLAGS=%CFLAGS% -Wall -Wextra
 
 set GLOBOBJS=dmx.c i_main.c i_ibm.c i_sound.c planar.o tables.c f_finale.c d_main.c d_net.c g_game.c m_menu.c m_misc.c am_map.c p_ceilng.c p_doors.c p_enemy.c p_floor.c p_inter.c p_lights.c p_map.c p_maputl.c p_plats.c p_pspr.c p_setup.c p_sight.c p_spec.c p_switch.c p_mobj.c p_telept.c p_tick.c p_user.c r_bsp.c r_data.c r_draw.c r_main.c r_plane.c r_segs.c r_things.c w_wad.c wi_stuff.c v_video.c st_lib.c st_stuff.c hu_stuff.c hu_lib.c s_sound.c z_zone.c info.c sounds.c dutils.c
