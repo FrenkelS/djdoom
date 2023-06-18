bb1e2e627fe71f38f4ccf8d89e0d31bfb102c9e1
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2023-06-18 11:38:46 +0200

    remove unused fx_man code

--- fx_man.h
+++ fx_man.h
@@ -1,5 +1,6 @@
 /*
 Copyright (C) 1994-1995 Apogee Software, Ltd.
+Copyright (C) 2023 Frenkel Smeijers
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -77,59 +78,17 @@ enum fx_BLASTER_Types
    };
 
 
-char *FX_ErrorString( int ErrorNumber );
-int   FX_SetupCard( int SoundCard, fx_device *device );
 int   FX_GetBlasterSettings( fx_blaster_config *blaster );
 int   FX_SetupSoundBlaster( fx_blaster_config blaster, int *MaxVoices, int *MaxSampleBits, int *MaxChannels );
 int   FX_Init( int SoundCard, int numvoices, int numchannels, int samplebits, unsigned mixrate );
 int   FX_Shutdown( void );
-int   FX_SetCallBack( void ( *function )( unsigned long ) );
 void  FX_SetVolume( int volume );
-int   FX_GetVolume( void );
 
-void  FX_SetReverseStereo( int setting );
-int   FX_GetReverseStereo( void );
-void  FX_SetReverb( int reverb );
-void  FX_SetFastReverb( int reverb );
-int   FX_GetMaxReverbDelay( void );
-int   FX_GetReverbDelay( void );
-void  FX_SetReverbDelay( int delay );
-
-int FX_VoiceAvailable( int priority );
-int FX_EndLooping( int handle );
 int FX_SetPan( int handle, int vol, int left, int right );
 int FX_SetPitch( int handle, int pitchoffset );
-int FX_SetFrequency( int handle, int frequency );
 
-int FX_PlayVOC( char *ptr, int pitchoffset, int vol, int left, int right,
-       int priority, unsigned long callbackval );
-int FX_PlayLoopedVOC( char *ptr, long loopstart, long loopend,
-       int pitchoffset, int vol, int left, int right, int priority,
-       unsigned long callbackval );
-int FX_PlayWAV( char *ptr, int pitchoffset, int vol, int left, int right,
-       int priority, unsigned long callbackval );
-int FX_PlayLoopedWAV( char *ptr, long loopstart, long loopend,
-       int pitchoffset, int vol, int left, int right, int priority,
-       unsigned long callbackval );
-int FX_PlayVOC3D( char *ptr, int pitchoffset, int angle, int distance,
-       int priority, unsigned long callbackval );
-int FX_PlayWAV3D( char *ptr, int pitchoffset, int angle, int distance,
-       int priority, unsigned long callbackval );
-int FX_PlayRaw( char *ptr, unsigned long length, unsigned rate,
-       int pitchoffset, int vol, int left, int right, int priority,
-       unsigned long callbackval );
-int FX_PlayLoopedRaw( char *ptr, unsigned long length, char *loopstart,
-       char *loopend, unsigned rate, int pitchoffset, int vol, int left,
-       int right, int priority, unsigned long callbackval );
-int FX_Pan3D( int handle, int angle, int distance );
+int FX_PlayRaw( char *ptr, unsigned long length, unsigned rate, int pitchoffset, int vol, int left, int right, int priority, unsigned long callbackval );
 int FX_SoundActive( int handle );
-int FX_SoundsPlaying( void );
 int FX_StopSound( int handle );
-int FX_StopAllSounds( void );
-int FX_StartDemandFeedPlayback( void ( *function )( char **ptr, unsigned long *length ),
-       int rate, int pitchoffset, int vol, int left, int right,
-       int priority, unsigned long callbackval );
-int  FX_StartRecording( int MixRate, void ( *function )( char *ptr, int length ) );
-void FX_StopRecord( void );
 
 #endif
