6d91e0546af448e791f9c72a0c4074061b8a441c
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2023-09-04 21:51:03 +0200

    just use 1 int64_t instead of 2 int32_t

--- w_wad.c
+++ w_wad.c
@@ -304,17 +304,16 @@ void W_InitMultipleFiles (char **filenames)
 int32_t	W_CheckNumForName (char *name)
 {
 	char	name8[9];
-	int32_t		v1,v2;
+	int64_t		v;
 	lumpinfo_t	*lump_p;
 
 // make the name into two integers for easy compares
 
 	strncpy (name8,name,8);
-	name8[8] = 0;			// in case the name was a fill 8 chars
+	name8[8] = 0;			// in case the name was a full 8 chars
 	strupr (name8);			// case insensitive
 
-	v1 = *(int32_t *)name8;
-	v2 = *(int32_t *)&name8[4];
+	v = *(int64_t *)name8;
 
 
 // scan backwards so patch lump files take precedence
@@ -322,7 +321,7 @@ int32_t	W_CheckNumForName (char *name)
 	lump_p = lumpinfo + numlumps;
 
 	while (lump_p-- != lumpinfo)
-		if ( *(int32_t *)lump_p->name == v1 && *(int32_t *)&lump_p->name[4] == v2)
+		if ( *(int64_t *)lump_p->name == v)
 			return lump_p - lumpinfo;
 
 
